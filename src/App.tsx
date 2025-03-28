0631aca2404bfb10e842355bed43f419e03b2898
an-kulinich <an.kulinich96@gmail.com>
2025-03-28 17:44:49 +0300

    Creation react project and adding card container

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import styled from 'styled-components';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Box>
+
+            </Box>
+        </div>
+    );
+}
+
+export default App;
+
+const Box = styled.div`
+    width: 300px;
+    height: 350px;
+    box-shadow: 0px 4px 20px 5px #0000001A;
+    border-radius: 15px;
+    background-color: #FFFFFF;
+`
